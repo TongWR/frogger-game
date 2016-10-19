ac9f544d3235128ae654497d06ec8d2d186d361c
Tong W. Ratanapan <tong.worapol@gmail.com>
2016-10-19 12:26:37 +0700

    refactor: Rewrite magic numbers in term of CELL_W, CELL_H, H_OFFSET

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
+/* Declare variables used across Player and Enemy's methods */
+var CELL_W = 101; // This value equals to image width
+var CELL_H = 83; // This is trickier; there's vertical overlap. The value 83 comes from render() in Engine
+var H_OFFSET = -30; // Determined by trial and error
+
 // Enemies our player must avoid
 var Enemy = function(initialX, initialY, speed) {
   this.sprite = 'images/enemy-bug.png';
 
   // Default initial location + speed provided if not supplied
-  this.x = initialX || -101;
-  this.y = initialY || 136;
+  this.x = initialX || -CELL_W;
+  this.y = initialY || (2*CELL_H + H_OFFSET);
   this.speed = speed || 100;
 };
 
@@ -15,14 +20,14 @@ Enemy.prototype.update = function(dt) {
   // which will ensure the game runs at the same speed for
   // all computers.
   if(this.isOffScreen()) {
-    this.x = -101;
+    this.x = -CELL_W;
   } else {
     this.x += this.speed * dt;
   }
 };
 
 Enemy.prototype.isOffScreen = function() {
-  return (this.x > 505) ? true : false;
+  return (this.x > 5*CELL_W) ? true : false;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -30,21 +35,12 @@ Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-/* Declare variables used across Player's methods */
-var CELL_W = 101; // This value equals to image width
-var CELL_H = 83; // This is trickier; there's vertical overlap. The value 83 comes from render() in Engine
-var H_OFFSET = -30; // Determined by trial and error
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
   this.sprite = 'images/char-horn-girl.png';
   this.setCanvasCoordinate(2, 5);
-};
-
-Player.prototype = function() {
-
 };
 
 Player.prototype.update = function() {
@@ -65,21 +61,21 @@ Player.prototype.handleInput = function(key) {
       }
       break;
     case 'up':
-      if(this.y === -30) {
+      if(this.y === H_OFFSET) {
         // At top cell, cannot move up; do nothing.
       } else {
         this.y -= CELL_H;
       }
       break;
     case 'right':
-      if(this.x === 404) {
+      if(this.x === 4*CELL_W) {
         // At rightmost cell, cannot move right; do nothing.
       } else {
         this.x += CELL_W;
       }
       break;
     case 'down':
-      if(this.y === 385) {
+      if(this.y === 5*CELL_H + H_OFFSET) {
         // At bottom cell, cannot move down; do nothing.
       } else {
         this.y += CELL_H;
@@ -94,14 +90,14 @@ Player.prototype.setCanvasCoordinate = function(cellX, cellY) {
 };
 
 Player.prototype.hasWon = function() {
-  return (this.y === -30) ? true : false;
+  return (this.y === -H_OFFSET) ? true : false;
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
 allEnemies[0] = new Enemy();
-allEnemies[1] = new Enemy(-101, 53, 500);
+allEnemies[1] = new Enemy(-CELL_W, CELL_H + H_OFFSET, 500);
 // Place the player object in a variable called player
 var player = new Player();
 
